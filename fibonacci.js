a4cc2eed7c9e60a3667825916a34f687a44eb702
AGN <itzme907@gmail.com>
2023-08-22 13:47:33 +0300

    Linting and removed comments

--- fibonacci.js
+++ fibonacci.js
@@ -1,22 +1,19 @@
 function fibs(n) {
-    let array = [0, 1];
+  const array = [0, 1];
 
   for (let i = 2; i < n; i++) {
     array.push(array[i - 1] + array[i - 2]);
   }
 
-  return array
-  }
-
-
-function fibsRec(n, array = [0, 1]) {
-  // if you're a hotshot 
-  return n < 3 ? array : array.push(array[array.length - 1] + array[array.length - 2])
-  //if (n < 3) return array
-
-  //array.push(array[array.length - 1] + array[array.length - 2])
-  //return fibsRec(n - 1, array)
+  return array;
 }
 
-console.log(fibs(8))
-console.log(fibsRec(8))
+function fibsRec(n, array = [0, 1]) {
+  if (n < 3) return array;
+
+  array.push(array[array.length - 1] + array[array.length - 2]);
+  return fibsRec(n - 1, array);
+}
+
+console.log(fibs(8));
+console.log(fibsRec(8));
